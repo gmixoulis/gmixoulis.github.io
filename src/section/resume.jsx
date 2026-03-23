359fe4177aedf5bccc25ec24411eeec4936433ff
George Michoulis <gmixoulis@gmail.com>
2026-03-23 22:15:02 +0100

    Refactor Resume component and update experience details

--- src/section/resume.jsx
+++ src/section/resume.jsx
@@ -64,7 +64,7 @@ const Resume = () => {
             <h3 className='s_border'>Experiences</h3>
             <ul className='d_timeline'>
               <li className='d_timeline-item'>
-                <h3 className='d_timeline-title'>2024 - now</h3>
+                <h3 className='d_timeline-title'>2024 - 2026</h3>
                 <div className='d_timeline-text'>
                   <span className='d_title'>
                     Blockchain Developer &amp; Researcher
